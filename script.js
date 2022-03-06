d3890f20e4a135463879e786a9e09d4bdf7087e7
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-03-07 00:38:35 +0300

    form validation function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ button.onclick = function(){
         button.innerHTML = "Collapse form";
     }
 };
+
+(function () {
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
