19289aa419b2961cc7c7359cb840f92483c466d2
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-03-07 14:08:43 +0300

    used if else to asign names

--- script.js
+++ script.js
@@ -42,4 +42,27 @@ button.onclick = function(){
 
 
 
+  if(days===0){
+    alert("your name is Kwasi")
+  }
+
+  else if(days===1){
+    alert("your name is Kwadwo")
+  }
+  else if(days===2){
+    alert("your name is Kwabena")
+  }
+  else if(days===3){
+    alert("your name is Kwaku")
+  }
+  else if(days===4){
+    alert("your name is Yaw")
+  }
+  else if(days===5){
+    alert("your name is Kofi")
+  }
+  else {
+    alert("your name is Kwame")
+  }
+
   }
